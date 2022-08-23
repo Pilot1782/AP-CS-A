bf630dd494e5c4161fae10fd4dcc6ae6f25032de
pilot1782 <71522520+Pilot1782@users.noreply.github.com>
2022-08-23 14:06:09 -0600

    fix

--- codingbat.java
+++ codingbat.java
@@ -5,8 +5,8 @@ public class codingbat {
 
 public static int sum3(int[] nums) {
     int res = 0;
-    for (int i=0;i<nums.length()i++;) {
-        res += i;
+    for (int i=0;i<nums.length;i++;) {
+        res += nums[i];
     }
     return res;
 }
