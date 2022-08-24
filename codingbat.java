b6507c3518d9bca32297a6963053d0bf68bac3df
pilot1782 <carson.bountry@gmail.com>
2022-08-23 22:08:41 -0600

    added stuff

--- codingbat.java
+++ codingbat.java
@@ -1,13 +1,25 @@
+/*
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+
+count7(717) → 2
+count7(7) → 1
+count7(123) → 0
+ */
+
 public class codingbat {
     public static void main (String[] args) {
-        System.out.println(sum3([1, 2, 3]) == 6, sum3([5, 11, 2]) == 18, sum3([7, 0, 0]) == 7)
+
     }
 
-public static int sum3(int[] nums) {
-    int res = 0;
-    for (int i=0;i<nums.length;i++;) {
-        res += nums[i];
+public static int count7(int n) {
+    int count = 0;
+    while (n > 0) {
+        if (n % 10 == 7) {
+            count++;
+        }
+        n /= 10;
     }
-    return res;
+    return count;
 }
 }
